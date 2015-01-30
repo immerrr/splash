5127797c2e8e8607c699f85c1d515a637d61007d
immerrr <immerrr@gmail.com>
2015-01-30 15:36:00 +0300

    Bump timeouts for benchmarking

--- splash/defaults.py
+++ splash/defaults.py
@@ -1,10 +1,10 @@
 
 # timeouts
-TIMEOUT = 30
+TIMEOUT = 300
 WAIT_TIME = 0.0
 
-MAX_TIMEOUT = 60.0
-MAX_WAIT_TIME = 10.0
+MAX_TIMEOUT = 600.0
+MAX_WAIT_TIME = 100.0
 
 # Default size of browser window.  As there're no decorations, this affects
 # both "window.inner*" and "window.outer*" values.
